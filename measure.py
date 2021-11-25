6fbcdc75fe6bfbcd34a40fc08dffcc719f5d8631
impromptus27 <impromptus27@gmail.com>
2021-11-25 13:50:22 +0800

    new

--- measure.py
+++ measure.py
@@ -60,15 +60,15 @@ def I_sub2(A,A2):
 average of the measures:
 """
 def avg_F1(Y,Y_true,X,X_true):
-    return 0.5(F1(Y,Y_true)+F1(X,X_true))
+    return 0.5*(F1(Y,Y_true)+F1(X,X_true))
 
 
 def avg_Icos(Y,Y_true,X,X_true):
-    return 0.5(I_cos(Y,Y_true)+I_cos(X,X_true))
+    return 0.5*(I_cos(Y,Y_true)+I_cos(X,X_true))
 
 
 def avg_Isub(Y,Y_true,X,X_true):
-    return 0.5(I_sub(Y,Y_true)+I_sub(X,X_true))
+    return 0.5*(I_sub(Y,Y_true)+I_sub(X,X_true))
 
 
 np.random.seed(1)
@@ -76,15 +76,26 @@ A = np.random.randint(2, size=(20,30))
 np.random.seed(31)
 A2 = np.random.randint(2, size=(20,30))
 
+
+np.random.seed(2)
+B = np.random.randint(2, size=(20,30))
+np.random.seed(32)
+B2 = np.random.randint(2, size=(20,30))
+
 #Am,An = A.shape
 #A2m,A2n = A2.shape
 
 
 print(A)
 print(A2)
+print(B)
+print(B2)
 print("F1 :",F1(A,A2))
 print("I_sub  :",I_sub(A,A2))
 print("I_sub2 :",I_sub2(A,A2))
 print("I_cos  :",I_cos(A,A2))
 print("I_cos2 :",I_cos2(A,A2))
+print("avg_F1 :",avg_F1(A,A2,B,B2))
+print("avg_Icos :",avg_Icos(A,A2,B,B2))
+print("avg_Isub :",avg_Isub(A,A2,B,B2))
 
